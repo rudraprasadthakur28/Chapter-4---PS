49fa1726c467dc5544f5d1416b0a3b598e061e2e
rudraprasadthakur28 <329627228+rudraprasadthakur28@users.noreply.github.com>
2026-09-21 13:07:36 +0530

    Add Chapter 4 PS

---
+++ problem2.py
@@ -0,0 +1,22 @@
+# marks = [55, 56, 89, 57, 85, 69]
+# marks.sort()
+
+# print(marks)
+
+marks = []
+
+f1 = int(input("Enter Marks Here : "))
+marks.append(f1)
+f2 = int(input("Enter Marks Here : "))
+marks.append(f2)
+f3 = int(input("Enter Marks Here : "))
+marks.append(f3)
+f4 = int(input("Enter Marks Here : "))
+marks.append(f4)
+f5 = int(input("Enter Marks Here : "))
+marks.append(f5)
+f6 = int(input("Enter Marks Here : "))
+marks.append(f6)
+
+marks.sort()
+print(marks)
---
+++ problem3.py
@@ -0,0 +1,4 @@
+a = (2, 4, "Rudra", "Thakur")
+
+a[2] = "Kolkata"
+print(a) # Tupple Cannot be changed
